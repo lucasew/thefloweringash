c8501b433ca1653c94693d9d1985b3ec4adcd657
Lucas Eduardo <lucas59356@gmail.com>
2020-11-29 10:34:50 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -10,113 +10,111 @@ let
        "aarch64-linux"
     ];
   };
-in
-{
-  exampleSystem = (pkgs.nixos {
+in (pkgs.nixos {
     fileSystems."/".device  = pkgs.lib.mkDefault "/dev/sda1";
     boot.loader.grub.device = pkgs.lib.mkDefault "/dev/sda";
+    environment.systemPackages = with pkgs; [
+       # essentials
+       bash
+       binutils
+       busybox
+       bzip2
+       coreutils
+       curl
+       gettext
+       nix
+       patchelf
+       stdenv
+       texinfo
+       which
+       xz
+       zlib
+
+       # sanity check
+       hello
+
+       # common interpreters, compilers and dev tools
+       autoconf
+       automake
+       bison
+       cargo
+       clang
+       clang_9 # current latest clang
+       expect
+       flex
+       ghc
+       go
+       guile
+       help2man
+       llvm
+       llvm_9 # current latest llvm
+       lua
+       perl
+       python2
+       python3
+       ruby
+       rustc
+       swig
+       tcl
+       tk
+
+       # common build systems and tools
+       cmake
+       meson
+       ninja
+       pkg-config
+
+       # commonly used tools
+       asciidoc
+       direnv
+       e2fsprogs
+       file
+       gdb
+       git
+       gnupg
+       ncdu
+       neofetch
+       pandoc
+       sqlite
+       sysstat
+       tcpdump
+       tio
+       tmux
+       tree
+       unzip
+       vim
+       xterm
+
+       # common libraries
+       cairo
+       db
+       expat
+       gmp
+       gnutls
+       isl
+       libelf
+       libffi
+       libmpc
+       libxml2
+       libxslt
+       mesa
+       mpfr
+       ncurses
+       npth
+       openssl
+       pango
+       pcre
+       readline
+
+       # kernels
+       linux
+       linux_latest
+
+       # nix specific tooling
+       cachix
+       lorri
+       nix-diff
+       ;
+     ];
   }).config.system.build.toplevel;
-
-  inherit (pkgs)
-    # essentials
-    bash
-    binutils
-    busybox
-    bzip2
-    coreutils
-    curl
-    gettext
-    nix
-    patchelf
-    stdenv
-    texinfo
-    which
-    xz
-    zlib
-
-    # sanity check
-    hello
-
-    # common interpreters, compilers and dev tools
-    autoconf
-    automake
-    bison
-    cargo
-    clang
-    clang_9 # current latest clang
-    expect
-    flex
-    ghc
-    go
-    guile
-    help2man
-    llvm
-    llvm_9 # current latest llvm
-    lua
-    perl
-    python2
-    python3
-    ruby
-    rustc
-    swig
-    tcl
-    tk
-
-    # common build systems and tools
-    cmake
-    meson
-    ninja
-    pkg-config
-
-    # commonly used tools
-    asciidoc
-    direnv
-    e2fsprogs
-    file
-    gdb
-    git
-    gnupg
-    ncdu
-    neofetch
-    pandoc
-    sqlite
-    sysstat
-    tcpdump
-    tio
-    tmux
-    tree
-    unzip
-    vim
-    xterm
-
-    # common libraries
-    cairo
-    db
-    expat
-    gmp
-    gnutls
-    isl
-    libelf
-    libffi
-    libmpc
-    libxml2
-    libxslt
-    mesa
-    mpfr
-    ncurses
-    npth
-    openssl
-    pango
-    pcre
-    readline
-
-    # kernels
-    linux
-    linux_latest
-
-    # nix specific tooling
-    cachix
-    lorri
-    nix-diff
-    ;
 }
