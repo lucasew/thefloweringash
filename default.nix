a66627264e4ce7c4ce0c0909d95459e609c126ce
Lucas Eduardo <lucas59356@gmail.com>
2020-11-29 12:51:24 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@ let
     };
   pkgs = import nixpkgs {
     overlays = [(self: super: {
-      inherit (pkgsArm) llvmPackages_7;
+      inherit (pkgsArm) llvmPackages_7 llvmPackages_9;
     })];
     crossSystem = {
       config = targetTriple;
