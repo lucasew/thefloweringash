063409837de8aecc4a57b38df990524981d176d6
Lucas Eduardo <lucas59356@gmail.com>
2020-11-29 18:33:58 -0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{target}:
+{target, triple}:
 let
   nixpkgs = builtins.fetchTarball {
     name = "nixpkgs-unstable";
@@ -7,112 +7,105 @@ let
   };
   pkgsOriginal = import nixpkgs {};
   pkgsCross = pkgsOriginal.pkgsCross.${target};
-in pkgsCross.stdenv.mkDerivation {
-   name = "packages";
-   src = ./.;
-   installPhase = ''
-      touch $out
-   '';
-   buildInputs =  with pkgsCross; [
-       # essentials
-       bash
-       binutils
-       busybox
-       bzip2
-       coreutils
-       curl
-       gettext
-       nix
-       patchelf
-       stdenv
-       texinfo
-       which
-       xz
-       zlib
+in map (pkg: "${pkg}") (with pkgsCross; [
+   # essentials
+   bash
+   binutils
+   busybox
+   bzip2
+   coreutils
+   curl
+   gettext
+   nix
+   patchelf
+   stdenv
+   texinfo
+   which
+   xz
+   zlib
 
-       # sanity check
-       hello
+   # sanity check
+   hello
 
-       # common interpreters, compilers and dev tools
-       autoconf
-       automake
-       bison
-       cargo
-       clang
-       clang_9 # current latest clang
-       expect
-       flex
-       ghc
-       go
-       guile
-       help2man
-       llvm
-       llvm_9 # current latest llvm
-       lua
-       perl
-       python2
-       python3
-       ruby
-       rustc
-       swig
-       tcl
-       tk
+   # common interpreters, compilers and dev tools
+   autoconf
+   automake
+   bison
+   cargo
+   clang
+   clang_9 # current latest clang
+   expect
+   flex
+   ghc
+   go
+   guile
+   help2man
+   llvm
+   llvm_9 # current latest llvm
+   lua
+   perl
+   python2
+   python3
+   ruby
+   rustc
+   swig
+   tcl
+   tk
 
-       # common build systems and tools
-       cmake
-       meson
-       ninja
-       pkg-config
+   # common build systems and tools
+   cmake
+   meson
+   ninja
+   pkg-config
 
-       # commonly used tools
-       asciidoc
-       direnv
-       e2fsprogs
-       file
-       gdb
-       git
-       gnupg
-       ncdu
-       neofetch
-       pandoc
-       sqlite
-       sysstat
-       tcpdump
-       tio
-       tmux
-       tree
-       unzip
-       vim
-       xterm
+   # commonly used tools
+   asciidoc
+   direnv
+   e2fsprogs
+   file
+   gdb
+   git
+   gnupg
+   ncdu
+   neofetch
+   pandoc
+   sqlite
+   sysstat
+   tcpdump
+   tio
+   tmux
+   tree
+   unzip
+   vim
+   xterm
 
-       # common libraries
-       cairo
-       db
-       expat
-       gmp
-       gnutls
-       isl
-       libelf
-       libffi
-       libmpc
-       libxml2
-       libxslt
-       mesa
-       mpfr
-       ncurses
-       npth
-       openssl
-       pango
-       pcre
-       readline
+   # common libraries
+   cairo
+   db
+   expat
+   gmp
+   gnutls
+   isl
+   libelf
+   libffi
+   libmpc
+   libxml2
+   libxslt
+   mesa
+   mpfr
+   ncurses
+   npth
+   openssl
+   pango
+   pcre
+   readline
 
-       # kernels
-       linux
-       linux_latest
+   # kernels
+   linux
+   linux_latest
 
-       # nix specific tooling
-       cachix
-       lorri
-       nix-diff
-     ];
-}
+   # nix specific tooling
+   cachix
+   lorri
+   nix-diff
+])
